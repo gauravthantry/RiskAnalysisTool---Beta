3ea27e6c5199155314d73bec9ce55cce3258b94d
Gaurav Thantry <gaurav.thantry@gmail.com>
2020-05-07 14:59:33 +1200

    Add TotalScore and associated operations to increment or decrement

--- js/app.js
+++ js/app.js
@@ -1,21 +1,38 @@
 $(document).ready(function () {
     let currentCategoryIndex = 0;
+    let totalScore = 0;
     $("#bGroup").hide();
-
+    $("#score").html("0");
     $("#next").click(function(){
      if(currentCategoryIndex == 0){
         console.log("true");
-           $("#aGroup").hide(200);
-           $("#bGroup").show(200);
+           $("#aGroup").fadeOut(1000, function(){
+            $("#bGroup").fadeIn(2000);
+           });
+           
            currentCategoryIndex++;
      }
     });
 
+      $(".ratingSelect").change(function(){
+         $("select").each(function(){
+            let selectionScore = parseInt($(this).val());
+            totalScore = totalScore + selectionScore;
+         });
+         $("#score").html(totalScore);
+         totalScore = 0;
+         $(this).blur();
+    });
+
+  
+
     $("#prev").click(function(){
         if(currentCategoryIndex == 1){
            console.log("true");
-              $("#bGroup").hide(200);
-              $("#aGroup").show(200);
+              $("#bGroup").fadeOut(1000, function(){
+               $("#aGroup").fadeIn(2000);
+              });
+              
               currentCategoryIndex--;
         }
        });
